4ba16d4ec593695db6b06feb97efb21cb1806721
coleh <coleh@coleh.net>
2020-12-02 10:41:27 -0500

    Added server-side saving with netlify functions

---
+++ functions/get.js
@@ -0,0 +1,33 @@
+exports.handler = async function (event, context) {
+    
+
+    var https = require("https");
+
+    const options = {
+        hostname: "jsonbin.org",
+        path: "/coleh2/swt/",
+        method: 'GET',
+        headers: { "Content-Type": "application/json", "Authorization": "token " + process.env.JSONBIN_API }
+    }
+
+    var req = https.request(options, function (res) {
+        res.setEncoding("utf8");
+
+        res.on("data", function (body) {
+            var jsonResBody = JSON.parse(body);
+            return {
+                statusCode: 201,
+                body: jsonResBody.length
+            };
+        });
+    });
+
+    req.on("error", function(err) {
+        return {
+            statusCode: 500,
+            body: err.message
+        }
+    });
+
+    req.end();
+}
